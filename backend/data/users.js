534f34c0f58ceaa0aef62942ae61481a15023ef4
Jigar Pancholi <jigarpancholi791@gmail.com>
2021-06-25 23:17:35 +0530

    Create schema and create api to fetch data from database

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin user',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('admin@123', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Jigar Pancholi',
+        email: 'jigar@example.com',
+        password: bcrypt.hashSync('admin@123', 10),
+        password: 'xxx',
+    }
+]
+
+export default users
